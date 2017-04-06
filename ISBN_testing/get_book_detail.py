9cd33cdc74ddbd9958e29408e43b8c0fe6c72d81
Wildfoot <wild.foot.yee.tzwu@gmail.com>
2017-04-06 21:19:10 +0800

    finished get book detail using isbn13

--- ISBN_testing/get_book_detail.py
+++ ISBN_testing/get_book_detail.py
@@ -1,15 +1,31 @@
 import urllib.request
+import json
 import settings
 def get_book_detail_using_isbn( input_isbn ):
-    my_request_url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(input_isbn) + "&key=" + str(settings.API_TOKEN)
-    print( my_request_url )
-    my_request = urllib.request.Request(my_request_url)
+    first_request_url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(input_isbn) + "&key=" + str(settings.API_TOKEN)
+    #print( first_request_url )
+    first_request = urllib.request.Request(first_request_url)
+    first_respond_data = ""
     try:
-        my_respond = urllib.request.urlopen(my_request)
-        my_respond_data = str(my_respond.read().decode('utf-8'))
-        print( my_respond_data )
+        first_respond = urllib.request.urlopen(first_request)
+        first_respond_data = str(first_respond.read().decode('utf-8'))
+        
     except Exception as e:
         print( str(e) )
 
-    
+    #print( first_respond_data )
+    #return first_respond_data
+    first_respond_json = json.loads( first_respond_data )
+    #return first_respond_json
+    second_request_url = first_respond_json["items"][0]["selfLink"]
+    second_request = urllib.request.Request(second_request_url)
+    second_respond_data = ""
+    try:
+        second_respond = urllib.request.urlopen(second_request)
+        second_respond_data = str(second_respond.read().decode('utf-8'))
+    except Exception as e:
+        print( str(e) )
+
+    second_respond_json = json.loads( second_respond_data )
+    return second_respond_json
 
