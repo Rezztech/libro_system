47b22dec14f6cfd3eb6065da12c231a52c4a7dff
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2017-04-08 20:37:57 +0800

    finished xISBN

--- ISBN_testing/xISBN_API.py
+++ ISBN_testing/xISBN_API.py
@@ -2,8 +2,9 @@ import urllib.request
 from write_error_log import append_error_log
 import json
 import settings
+
 def get_book_detail_using_isbn( input_isbn ):
-    first_request_url = "http://xisbn.worldcat.org/webservices/xid/isbn/" + str(input_isbn) + "?method=getEditions&format=json&fl=publisher,author,year,title" )
+    first_request_url = "http://xisbn.worldcat.org/webservices/xid/isbn/" + str(input_isbn) + "?method=getEditions&format=json&fl=publisher,author,year,title"
     first_request = urllib.request.Request(first_request_url)
     first_respond_data = ""
     try:
@@ -22,27 +23,30 @@ def get_book_detail_using_isbn( input_isbn ):
         print( "Can't find any item!" )
         return False
 
-    second_respond_json = first_respond_json
-    return_item = {}
-    try:
-        respond_item = second_respond_json["volumeInfo"]
-        return_item["title"] = respond_item["title"] if "title" in respond_item else ""
-        return_item["subtitle"] = respond_item["subtitle"] if "subtitle" in respond_item else ""
-        return_item["authors"] = respond_item["authors"] if "authors" in respond_item else []
-        return_item["publisher"] = respond_item["publisher"] if "publisher" in respond_item else ""
-        return_item["publishedDate"] = respond_item["publishedDate"] if "publishedDate" in respond_item else ""
-        return_item["industryIdentifiers"] = respond_item["industryIdentifiers"] if "industryIdentifiers" in respond_item else []
-        return_item["description"] = respond_item["description"] if "description" in respond_item else ""
-    except Exception as e:
-        append_error_log( "while fill up respond_item " + str(input_isbn) )
-        append_error_log( str(e) )
-        print( str(e) )
-        return False
-    return return_item
+    return first_respond_json["list"]
 
 def get_book_detail( request_bar ):
     return_object = {}
-    return_object["TotalItems"] = 1
+    respond_items = get_book_detail_using_isbn( request_bar )
+    return_object["TotalItems"] = len(respond_items)
     return_object["items"] = []
-    return_object["items"].append( get_book_detail_using_isbn( request_bar ))
+    for item in respond_items:
+        temp_return_item = {}
+        try:
+            temp_return_item["title"] = item["title"] if "title" in item else ""
+            temp_return_item["subtitle"] = item["subtitle"] if "subtitle" in item else ""
+            temp_return_item["authors"] = item["authors"] if "authors" in item else []
+            temp_return_item["publisher"] = item["publisher"] if "publisher" in item else ""
+            temp_return_item["publishedDate"] = item["year"] if "year" in item else ""
+            temp_return_item["industryIdentifiers"] = []
+            if "isbn" in item:
+                for one_isbn in item["isbn"]:
+                    temp_return_item["industryIdentifiers"].append( { "type": "ISBN_13", "identifier": one_isbn } )
+            temp_return_item["description"] = item["description"] if "description" in item else ""
+        except Exception as e:
+            append_error_log( "while fill up temp_return_item " + str(request_bar) )
+            append_error_log( str(e) )
+            print( str(e) )
+            return False
+        return_object["items"].append( temp_return_item )
     return return_object
