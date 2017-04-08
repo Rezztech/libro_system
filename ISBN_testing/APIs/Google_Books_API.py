a5702544a5c0cf31aa87931d7781217fa5610efd
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2017-04-08 21:38:03 +0800

    improve

--- ISBN_testing/APIs/Google_Books_API.py
+++ ISBN_testing/APIs/Google_Books_API.py
@@ -18,49 +18,33 @@ def get_book_detail_using_isbn( input_isbn ):
 
     first_respond_json = json.loads( first_respond_data )
     #return first_respond_json
-    if first_respond_json["totalItems"] == 0:
-        print( "Can't find any item!" )
-        return False
-    
-    if first_respond_json["totalItems"] != 1:
-        print( "find items more than 1!!!!!!!!!!" )
-        append_error_log("Google Books API find more than one respond while isbn = " + input_isbn)
 
-    second_request_url = first_respond_json["items"][0]["selfLink"]
-    second_request = urllib.request.Request(second_request_url)
-    second_respond_data = ""
-    try:
-        second_respond = urllib.request.urlopen(second_request)
-        second_respond_data = str(second_respond.read().decode('utf-8'))
-    except Exception as e:
-        append_error_log( "while request + " + second_request_url )
-        append_error_log( str(e) )
-        print( str(e) )
-        return False
+    return_object = {}
+    return_object["TotalItems"] = first_respond_json["totalItems"]
+    return_object["items"] = first_respond_json["items"] if "items" in first_respond_json else []
 
-    second_respond_json = json.loads( second_respond_data )
-    #return second_respond_json
-    return_item = {}
-    try:
-        respond_item = second_respond_json["volumeInfo"]
-        return_item["title"] = respond_item["title"] if "title" in respond_item else ""
-        return_item["subtitle"] = respond_item["subtitle"] if "subtitle" in respond_item else ""
-        return_item["authors"] = respond_item["authors"] if "authors" in respond_item else []
-        return_item["publisher"] = respond_item["publisher"] if "publisher" in respond_item else ""
-        return_item["publishedDate"] = respond_item["publishedDate"] if "publishedDate" in respond_item else ""
-        return_item["industryIdentifiers"] = respond_item["industryIdentifiers"] if "industryIdentifiers" in respond_item else []
-        return_item["description"] = respond_item["description"] if "description" in respond_item else ""
-    except Exception as e:
-        append_error_log( "while fill up respond_item " + str(input_isbn) )
-        append_error_log( str(e) )
-        print( str(e) )
-        return False
-    return return_item
+    return return_object
 
 def get_book_detail( request_bar ):
     return_object = {}
-    return_object["TotalItems"] = 1
+    respond_object = get_book_detail_using_isbn( request_bar )
+    return_object["TotalItems"] = respond_object["TotalItems"]
     return_object["items"] = []
-    return_object["items"].append( get_book_detail_using_isbn( request_bar ))
+    for item in respond_object["items"]:
+        temp_return_object = {}
+        try:
+            respond_item = item["volumeInfo"]
+            temp_return_object["title"] = respond_item["title"] if "title" in respond_item else ""
+            temp_return_object["subtitle"] = respond_item["subtitle"] if "subtitle" in respond_item else ""
+            temp_return_object["authors"] = respond_item["authors"] if "authors" in respond_item else []
+            temp_return_object["publisher"] = respond_item["publisher"] if "publisher" in respond_item else ""
+            temp_return_object["publishedDate"] = respond_item["publishedDate"] if "publishedDate" in respond_item else ""
+            temp_return_object["industryIdentifiers"] = respond_item["industryIdentifiers"] if "industryIdentifiers" in respond_item else []
+            temp_return_object["description"] = respond_item["description"] if "description" in respond_item else ""
+        except Exception as e:
+            append_error_log( "while fill up temp_return_object " + str( request_bar ) + " (Google Books API)" )
+            append_error_log( str(e) )
+            return_object["items"].append( temp_return_object )
+   
     return return_object
 
