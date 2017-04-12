a099675142fb924cf7b3e300c9921fec404a3cba
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2017-04-12 09:12:09 +0800

    add static & templates folder and settings

--- libro_system/settings.py
+++ libro_system/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'libro_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
