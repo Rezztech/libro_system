92cc8b7fe5099ba7b313265e45094cbce95b3d07
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2017-04-14 23:44:26 +0800

    del ISBN_testing and add db.sqlite3 in .gitignore

--- ISBN_testing/write_error_log.py
+++
@@ -1,6 +0,0 @@
-import datetime
-def append_error_log( error_message ):
-    Error_log_file = open("Error_log","a",encoding='utf8')
-    Error_log_file.write( datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + error_message + "\n" )
-    Error_log_file.close()
-
