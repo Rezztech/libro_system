f3c098a8ce87994c018bcb101ae006f14ba8c4f8
soytw <soytwww@gmail.com>
2017-04-15 13:06:36 +0800

    move isbn_issn_validator

--- books/models.py
+++ books/models.py
@@ -11,46 +11,7 @@ import re
 
 
 def isbn_issn_validator(string):
-    def valid(string):
-        if (re.fullmatch(r'[0-9]{9}[0-9X]', string)
-                or re.fullmatch(r'[0-9]{13}', string)
-                or re.fullmatch(r'[0-9]{7}[0-9X]', string)):
-            if len(string) == 10:
-                # ISBN-10
-                weight = (10, 9, 8, 7, 6, 5, 4, 3, 2)
-                s = sum(map(operator.mul, map(int, string[:-1]), weight))
-                n = 11 - (s % 11)
-                if n == 10:
-                    return string[-1] == 'X'
-                elif n == 11:
-                    return string[-1] == '0'
-                else:
-                    return string[-1] == str(n)
-
-            elif len(string) == 13:
-                # ISBN-13
-                weight = (1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3)
-                s = sum(map(operator.mul, map(int, string[:-1]), weight))
-                n = 10 - (s % 10)
-                if n == 10:
-                    n = 0
-                return string[-1] == str(n)
-
-            elif len(string) == 8:
-                # ISSN
-                weight = (8, 7, 6, 5, 4, 3, 2)
-                s = sum(map(operator.mul, map(int, string[:-1]), weight))
-                n = 11 - (s % 11)
-                if n == 11:
-                    return string[-1] == '0'
-                elif n == 10:
-                    return string[-1] == 'X'
-                else:
-                    return string[-1] == str(n)
-        else:
-            return False
-
-    if not valid(string):
+    if not utils.check_isbn_issn(string):
         raise ValidationError()
 
 
--- books/utils.py
+++ books/utils.py
@@ -2,13 +2,44 @@ from urllib.request import urlopen
 import json
 import datetime
 
-def get_book_info_online(isbn):
-    # [TODO] handle duplicated items in response
-    response1 = json.loads(
-        urlopen('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
-        .read().decode())
-    response2 = json.loads(urlopen(response1['items'][0]['selfLink']).read().decode())
-    return response2['volumeInfo']
+def check_isbn_issn(string):
+    if (re.fullmatch(r'[0-9]{9}[0-9X]', string)
+            or re.fullmatch(r'[0-9]{13}', string)
+            or re.fullmatch(r'[0-9]{7}[0-9X]', string)):
+        if len(string) == 10:
+            # ISBN-10
+            weight = (10, 9, 8, 7, 6, 5, 4, 3, 2)
+            s = sum(map(operator.mul, map(int, string[:-1]), weight))
+            n = 11 - (s % 11)
+            if n == 10:
+                return string[-1] == 'X'
+            elif n == 11:
+                return string[-1] == '0'
+            else:
+                return string[-1] == str(n)
+
+        elif len(string) == 13:
+            # ISBN-13
+            weight = (1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3)
+            s = sum(map(operator.mul, map(int, string[:-1]), weight))
+            n = 10 - (s % 10)
+            if n == 10:
+                n = 0
+            return string[-1] == str(n)
+
+        elif len(string) == 8:
+            # ISSN
+            weight = (8, 7, 6, 5, 4, 3, 2)
+            s = sum(map(operator.mul, map(int, string[:-1]), weight))
+            n = 11 - (s % 11)
+            if n == 11:
+                return string[-1] == '0'
+            elif n == 10:
+                return string[-1] == 'X'
+            else:
+                return string[-1] == str(n)
+    else:
+        return False
 
 def parse_date(date_str):
 	l = list(map(int, date_str.split('-')))
