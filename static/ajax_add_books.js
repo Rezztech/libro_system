2ecafa8fcbb0f4db29152e826dd673c3d4841912
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2017-04-15 11:11:53 +0800

    improve some user experience

--- static/ajax_add_books.js
+++ static/ajax_add_books.js
@@ -1,4 +1,3 @@
-var TEST
 function create_isbn_select(Identifier_type){
     var isbn_select = "";
     isbn_select += '<select name="identifier">\n';
@@ -8,59 +7,85 @@ function create_isbn_select(Identifier_type){
     isbn_select += '</select>';
     return isbn_select
 }
+function isbn_to_book_detail_ajax(){
+    $(".my-new-book-detail").remove();
+    $.ajax({
+        url: "/add-books-ajax/",
+        type: "POST",
+        data: {
+            isbn_input: $("#isbn-input").val(),
+        },
+        error: function() {
+            alert("Ajax request error");
+        },
+        success: function( response ){
+            for(var i = 0;i < response["TotalItems"];i++ )
+            {
+                E = response["items"][i];
+                var display_item = [];
+                display_item.push("title : ");
+                display_item.push('<input type="text" name="title" value="' + E["title"] + '"> <br>');
+                display_item.push("subtitle : ");
+                display_item.push('<input type="text" name="subtitle" value="' + E["subtitle"] + '"> <br>');
+                display_item.push("authors : ");
+                display_item.push('add');
+                display_item.push('<br>');
+                for (var j in E["authors"]){
+                    display_item.push('<input type="text" name="author" value="' + E["authors"][j] + '">');
+                    display_item.push('<span class="remove-author remove">✗</span> <br>')
+                }
+                display_item.push("publisher : ");
+                display_item.push('<input type="text" name="publisher" value="' + E["publisher"] + '"> <br>');
+                display_item.push("publisheddate : ");
+                display_item.push('<input type="text" name="publisheddate" value="' + E["publishedDate"] + '"> <br>');
+                display_item.push("identifier : ");
+                display_item.push('add');
+                display_item.push('<br>');
+                for(var j in E["industryIdentifiers"])
+                {
+                    display_item.push(create_isbn_select(E["industryIdentifiers"][j]["type"]))
+                    display_item.push('<input type="text" name="" value="' + E["industryIdentifiers"][j]["identifier"] + '">');
+                    display_item.push('<span class="remove-identifier remove">✗</span> <br>')
+                }
+                display_item.push("description : ");
+                display_item.push('<input type="textarea" name="description" value="' + E["description"] + '"> <br>');
+
+                $("<div/>", {
+                "class": "my-new-book-detail",
+                html: display_item.join("")
+                }).appendTo("body");
+            }
+        }
+    });
+}
+
+/*
+function active_remove_click(){
+    $(".remove-author").click(function(){
+        console.log("YOOOOOOOOOOOOOOOOOOOOO");
+        $("this").remove();
+    });
+
+}
+*/
 
 $(document).ready(function(){
+    $("#isbn-input").focus();
     $("#sent-isbn").click(function(){
-        $(".my-new-book-detail").remove();
-        $.ajax({
-            url: "/add-books-ajax/",
-            type: "POST",
-            data: {
-                isbn_input: $("#isbn-input").val(),
-            },
-            error: function() {
-                alert("Ajax request error");
-            },
-            success: function( response ){
-                TEST = response;
-                for(var i = 0;i < response["TotalItems"];i++ )
-                {
-                    E = response["items"][i];
-                    var display_item = [];
-                    display_item.push("title : ");
-                    display_item.push('<input type="text" name="title" value="' + E["title"] + '"> <br>');
-                    display_item.push("subtitle : ");
-                    display_item.push('<input type="text" name="subtitle" value="' + E["subtitle"] + '"> <br>');
-                    display_item.push("authors : ");
-                    display_item.push('add');
-                    display_item.push('<br>');
-                    for (var j in E["authors"]){
-                        display_item.push('<input type="text" name="author" value="' + E["authors"][j] + '">');
-                    display_item.push('X<br>');
-                    }
-                    display_item.push("publisher : ");
-                    display_item.push('<input type="text" name="publisher" value="' + E["publisher"] + '"> <br>');
-                    display_item.push("publisheddate : ");
-                    display_item.push('<input type="text" name="publisheddate" value="' + E["publishedDate"] + '"> <br>');
-                    display_item.push("identifier : ");
-                    display_item.push('add');
-                    display_item.push('<br>');
-                    for(var j in E["industryIdentifiers"])
-                    {
-                        display_item.push(create_isbn_select(E["industryIdentifiers"][j]["type"]))
-                        display_item.push('<input type="text" name="" value="' + E["industryIdentifiers"][j]["identifier"] + '">');
-                        display_item.push('X<br>')
-                    }
-                    display_item.push("description : ");
-                    display_item.push('<input type="textarea" name="description" value="' + E["description"] + '"> <br>');
-
-                    $("<div/>", {
-                    "class": "my-new-book-detail",
-                    html: display_item.join("")
-                    }).appendTo("body");
-                }
-            }
-        });
+        isbn_to_book_detail_ajax();
     });
+    $("#isbn-input").keypress(function(keyin){
+        if(keyin.which == 13){
+            isbn_to_book_detail_ajax();
+            return false;
+        }
+    });
+    /* fail
+    $(document).on("click", ".remove-author", function(){
+        active_remove_click();
+        console.log("BYOOOOOOOOOOOOOOOOOOOOO");
+        //$(this)
+    })
+    */
 });
 
