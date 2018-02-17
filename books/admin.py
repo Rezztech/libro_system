c04cf998c1ac76e80fb27e577a06d74e96a355ee
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-02-17 13:30:32 +0800

    add identifier inline function into bookdetails

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from books.models import Bookdetails, Book
+from books.models import Bookdetails, Book, BookIdentifier
+
+class Identifier_inline(admin.TabularInline):
+    model = BookIdentifier
+
+class BookdetailAdmin(admin.ModelAdmin):
+    list_display = ("title", "publisher")
+    search_fields = ("name", "subtitle", "publisher", "description")
+    inlines = [ Identifier_inline, ]
+
 
 # Register your models here.
-admin.site.register(Bookdetails)
+admin.site.register(Bookdetails, BookdetailAdmin)
 admin.site.register(Book)
 
